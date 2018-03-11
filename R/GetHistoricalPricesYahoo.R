bd0610d3771f19ffd4b00d7dca0dfb2f711aa87d
jspinney-nbimc <jspinney@nbimc.com>
2018-03-11 10:07:20 -0300

    GetHistoricalPricesYahoo() added

---
+++ R/GetHistoricalPricesYahoo.R
@@ -0,0 +1,29 @@
+GetHistoricalPricesYahoo <- function( tickers , start_date , end_date ) {
+  if (length(tickers) > 1) {
+    data <- tseries::get.hist.quote(instrument=tickers[1],start=start_date,
+                                    end=end_date,
+                                    quote=c("Adjusted"),
+                                    provider="yahoo",
+                                    retclass="zoo",
+                                    quiet=T)
+    for (i in 2:length(tickers)) {
+      hist <- tseries::get.hist.quote(instrument=tickers[i],
+                                      start=start_date,
+                                      end=end_date,
+                                      quote=c("Adjusted"),
+                                      provider="yahoo",
+                                      retclass="zoo",
+                                      quiet=T)
+      data <- merge(data,hist)}
+    colnames(data) <- tickers
+  } else {
+    data <- tseries::get.hist.quote(instrument=tickers[1],start=start_date,
+                                    end=end_date,
+                                    quote=c("Adjusted"),
+                                    provider="yahoo",
+                                    retclass="zoo",
+                                    quiet=T)
+    colnames(data) <- tickers
+  }
+  return(data)
+}
