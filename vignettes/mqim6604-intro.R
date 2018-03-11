9ad3e6d441d810956edd133d3106348af48539cf
jspinney-nbimc <jspinney@nbimc.com>
2018-03-11 08:53:17 -0300

    html vignette

---
+++ vignettes/mqim6604-intro.R
@@ -0,0 +1,15 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----eval=FALSE----------------------------------------------------------
+#  # if devtools not installed
+#  #install.packages("devtools")
+#  #library(devtools)
+#  install_github("jspinney-nbimc/mqim6604")
+
+## ----eval=FALSE----------------------------------------------------------
+#  library(mqim6604)
+
