0929bef5f322f5b47a8f0bdbfbd89984bc07f345
jspinney-nbimc <jspinney@nbimc.com>
2018-03-18 20:09:56 -0300

    cdn_stocks data set documentation

---
+++ R/CDN_LC_STOCKS_DESC_DATA.R
@@ -0,0 +1,15 @@
+#' CDN_LC_STOCKS_DESC_DATA
+#'
+#' Descriptive data (sector, mkt cap, etc) for a random sample of 50
+#' Canadian stocks. Matched with the returns data set.
+#'
+#' @docType data
+#'
+#' @usage data(CDN_LC_STOCKS_DESC_DATA)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(CDN_LC_STOCKS_DESC_DATA)
+#'
+"CDN_LC_STOCKS_DESC_DATA"
---
+++ R/CDN_LC_STOCKS_RETURN_DATA.R
@@ -0,0 +1,16 @@
+#' CDN_LC_STOCKS_RETURN_DATA
+#'
+#' Daily total returns (gross of dividends) for a random sample of 50
+#' Canadian stocks over the period  2015-01-02 to 2018-02-28. May
+#' contain N/A observations.
+#'
+#' @docType data
+#'
+#' @usage data(CDN_LC_STOCKS_RETURN_DATA)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(CDN_LC_STOCKS_RETURN_DATA)
+#'
+"CDN_LC_STOCKS_RETURN_DATA"
